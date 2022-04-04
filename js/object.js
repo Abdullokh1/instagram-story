8edf477611dfd63751ecd2ae1a269e350a60eaba
Abdullokh1 <giyasovabdullo0@gmail.com>
2022-04-04 13:09:43 +0500

    initial commit

---
+++ js/object.js
@@ -0,0 +1,32 @@
+let media = [
+  {
+    name: 'Iqtiboslar',
+    img:  ['https://picsum.photos/id/125/389/1000'],
+    id: 1
+  },
+  {
+    name: 'Fikrlar',
+    img:  ['https://picsum.photos/id/452/389/1000'],
+    id: 2
+  },
+  {
+    name: 'Dokonlarimiz',
+    img:  ['https://picsum.photos/id/25/389/1000'],
+    id: 3
+  },
+  {
+    name: 'Blog',
+    img:  ['https://picsum.photos/id/635/389/1000'],
+    id: 4
+  },
+  {
+    name: 'Location',
+    img:  ['https://picsum.photos/id/45/389/1000'],
+    id: 5
+  },
+  {
+    name: 'Iqtiboslar',
+    img:  ['https://picsum.photos/id/115/389/1000'],
+    id: 6
+  },
+]
